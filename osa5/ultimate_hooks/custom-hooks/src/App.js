b4f39c9ca0510737c0dc33e4d59729a71e802ccd
Jan Nyman <jan.nyman@valagroup.com>
2019-11-18 14:34:06 +0200

    Small improvements

--- osa5/ultimate_hooks/custom-hooks/src/App.js
+++ osa5/ultimate_hooks/custom-hooks/src/App.js
@@ -61,12 +61,14 @@ const App = () => {
     noteService
       .getAll()
       .then(initialNotes => noteService.addResources(initialNotes))
+    // eslint-disable-next-line
   }, [])
 
   useEffect(() => {
     personService
       .getAll()
       .then(initialPersons => personService.addResources(initialPersons))
+    // eslint-disable-next-line
   }, [])
 
   const handleNoteSubmit = (event) => {
@@ -88,19 +90,29 @@ const App = () => {
       })
   }
 
+  const removeReset = (object) => {
+    // eslint-disable-next-line no-unused-vars
+    const { reset, ...rest } = object
+    return rest
+  }
+
+  const note = removeReset(content)
+  const person = removeReset(name)
+  const phonenumber = removeReset(number)
+
   return (
     <div>
       <h2>notes</h2>
       <form onSubmit={handleNoteSubmit}>
-        <input {...content} />
+        <input {...note} />
         <button>create</button>
       </form>
       {notes.map(n => <p key={n.id}>{n.content}</p>)}
 
       <h2>persons</h2>
       <form onSubmit={handlePersonSubmit}>
-        name <input {...name} /> <br />
-        number <input {...number} />
+        name <input {...person} /> <br />
+        number <input {...phonenumber} />
         <button>create</button>
       </form>
       {persons.map(n => <p key={n.id}>{n.name} {n.number}</p>)}
