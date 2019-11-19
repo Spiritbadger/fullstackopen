6458e8591b1992de349f32015481b2e31b86b0b4
Jan Nyman <jan.nyman@valagroup.com>
2019-11-19 10:05:59 +0200

    Voting and adding anecdote

--- osa6/redux-anecdotes/src/App.js
+++ osa6/redux-anecdotes/src/App.js
@@ -1,10 +1,31 @@
 import React from 'react';
 
+const generateId = () =>
+  Number((Math.random() * 1000000).toFixed(0))
+
 const App = (props) => {
   const anecdotes = props.store.getState()
 
   const vote = (id) => {
     console.log('vote', id)
+    props.store.dispatch({
+      type: 'VOTE',
+      data: { id }
+    })
+  }
+
+  const addAnecdote = (event) => {
+    event.preventDefault()
+    const content = event.target.anecdote.value
+    props.store.dispatch({
+      type: 'NEW_ANECDOTE',
+      data: {
+        content,
+        votes: 0,
+        id: generateId()
+      }
+    })
+    event.target.anecdote.value = ''
   }
 
   return (
@@ -22,9 +43,9 @@ const App = (props) => {
         </div>
       )}
       <h2>create new</h2>
-      <form>
-        <div><input /></div>
-        <button>create</button>
+      <form onSubmit={addAnecdote}>
+        <input name="anecdote" />
+        <button type="submit">create</button>
       </form>
     </div>
   )
